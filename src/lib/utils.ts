477ef1792e8eb1eecbfe60be8ba4084740243ea3
Khoirul Asfian <mk.asfian@gmail.com>
2025-10-28 23:37:24 +0700

    feat: update entry card & add external link

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+dayjs.extend(relativeTime);
+export function formatRelativeDate(date: Date) {
+	return dayjs(date).fromNow();
 }
--- src/routes/reader/index.tsx
+++ src/routes/reader/index.tsx
@@ -4,7 +4,7 @@ import { useServerFn } from '@tanstack/react-start';
 import { getEntryServerFn } from '@/lib/server/entry-sfn';
 import { Loader } from '@/components/ui/loader';
 import { Button } from '@/components/ui/button';
-import { IconX } from '@intentui/icons';
+import { IconOpenLink, IconX } from '@intentui/icons';
 
 export const Route = createFileRoute('/reader/')({
 	component: RouteComponent
@@ -28,7 +28,7 @@ function RouteComponent() {
 	};
 
 	return (
-		<div className="grid grid-cols-1 justify-center gap-y-1">
+		<div className="mx-auto grid max-w-2xl grid-cols-1 justify-center gap-y-1">
 			<Button
 				size="sq-sm"
 				intent="outline"
@@ -41,18 +41,36 @@ function RouteComponent() {
 			{res.status === 'error' && 'Error'}
 			{res.status === 'success' && (
 				<>
-					<div className="mx-auto mb-1 flex w-full max-w-2xl items-center gap-x-2 text-sm text-foreground/75">
+					<div className="mx-auto flex w-full items-center gap-x-2 text-sm text-foreground/75">
 						<a href={res.data.feed.site_url} className="text-primary hover:underline">
 							{res.data.feed.title}
 						</a>
 						<span className="h-full w-px bg-foreground/50" />
 						<span>{res.data.author}</span>
+						<span className="h-full w-px bg-foreground/50" />
+						<span>
+							{new Date(res.data.published_at).toLocaleString(['en-SG', 'en-US'], {
+								day: 'numeric',
+								month: 'long',
+								year: 'numeric',
+								hour: 'numeric',
+								minute: 'numeric'
+							})}
+						</span>
+						<span className="h-full w-px bg-foreground/50" />
+						<a
+							href={res.data.url}
+							target="_blank"
+							className="inline-flex items-center gap-x-1 text-primary hover:underline"
+						>
+							External Link <IconOpenLink />
+						</a>
 					</div>
-					<h1 className="mx-auto max-w-2xl text-3xl leading-[105%] font-bold text-pretty">
+					<h1 className="mx-auto my-4 w-full text-3xl leading-[105%] font-bold text-pretty">
 						{res.data?.title}
 					</h1>
 					<div
-						className="mx-auto prose mt-4 max-w-2xl prose-neutral dark:prose-invert prose-a:decoration-accent prose-a:hover:decoration-foreground prose-img:rounded-lg prose-img:border prose-img:border-border prose-img:shadow-xs"
+						className="mx-auto prose max-w-2xl prose-neutral dark:prose-invert prose-a:decoration-accent prose-a:hover:decoration-foreground prose-img:rounded-xl prose-img:border prose-img:border-border prose-img:shadow-sm prose-img:shadow-accent/50"
 						dangerouslySetInnerHTML={{ __html: res.data?.content }}
 					/>
 				</>
