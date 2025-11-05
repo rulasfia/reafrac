b230ee57baae6172ced3d8c921c4af08f778033d
Khoirul Asfian <mk.asfian@gmail.com>
2025-11-05 16:35:45 +0700

    feat: add saved entry management

--- src/routes/reader/index.tsx
+++ src/routes/reader/index.tsx
@@ -1,10 +1,15 @@
 import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useServerFn } from '@tanstack/react-start';
-import { getEntryContentServerFn, getEntryServerFn } from '@/lib/server/entry-sfn';
+import {
+	getEntryContentServerFn,
+	getEntryServerFn,
+	saveEntryToBookmarkServerFn
+} from '@/lib/server/entry-sfn';
 import { Loader } from '@/components/ui/loader';
 import { Button, buttonStyles } from '@/components/ui/button';
-import { IconOpenLink, IconX } from '@intentui/icons';
+import { IconBookmark, IconBookmarkFill, IconOpenLink, IconX } from '@intentui/icons';
+import { toast } from 'sonner';
 
 export const Route = createFileRoute('/reader/')({
 	component: RouteComponent
@@ -12,8 +17,10 @@ export const Route = createFileRoute('/reader/')({
 
 function RouteComponent() {
 	const search = Route.useSearch();
+	const qc = useQueryClient();
 	const getEntry = useServerFn(getEntryServerFn);
 	const getEntryContent = useServerFn(getEntryContentServerFn);
+	const saveEntryToBookmark = useServerFn(saveEntryToBookmarkServerFn);
 	const navigate = useNavigate({ from: Route.fullPath });
 
 	const entry = useQuery({
@@ -31,6 +38,19 @@ function RouteComponent() {
 
 	const onCloseReader = () => {
 		navigate({ search: (prev) => ({ ...prev, entry: undefined }) });
+	};
+
+	const onSaveToBookmark = async () => {
+		try {
+			if (!entry.data) return;
+			const newValue = !entry.data.starred;
+			await saveEntryToBookmark({ data: { entryId: entry.data.id, saved: newValue } });
+			await qc.invalidateQueries({ queryKey: ['entry', search.entry] });
+			toast.success(newValue ? 'Added to bookmark' : 'Removed from bookmark');
+		} catch (error) {
+			console.error(error);
+			toast.error('Failed to update bookmark');
+		}
 	};
 
 	if (!search.entry) {
@@ -71,18 +91,46 @@ function RouteComponent() {
 								minute: 'numeric'
 							})}
 						</span>
-						<span className="h-full w-px bg-foreground/50" />
-						<a
-							href={entry.data.link}
-							target="_blank"
-							className="inline-flex items-center gap-x-1 text-primary hover:underline"
-						>
-							Source Link <IconOpenLink />
-						</a>
 					</div>
-					<h1 className="mx-auto my-4 w-full text-3xl leading-[105%] font-bold text-pretty">
+					<h1 className="mx-auto mt-4 mb-3 w-full text-3xl leading-[105%] font-bold text-pretty">
 						{entry.data?.title}
 					</h1>
+
+					<div className="mb-4 flex items-center">
+						{/*<Link
+							className={buttonStyles({
+								intent: 'outline',
+								className: 'mt-4 w-fit cursor-pointer rounded-r-none'
+							})}
+							to="/reader"
+							search={{ ...search, view: search.view === 'expanded' ? undefined : 'expanded' }}
+						>
+							{search.view === 'expanded' && content.status === 'success'
+								? 'Back to Summary'
+								: 'Read Full Article'}
+							{content.isLoading ? <Loader /> : null}
+						</Link>*/}
+						<Button
+							intent="outline"
+							className="w-fit cursor-pointer rounded-r-none"
+							onClick={onSaveToBookmark}
+						>
+							{entry.data.starred ? <IconBookmarkFill /> : <IconBookmark />}
+							Save
+						</Button>
+						<a
+							className={buttonStyles({
+								intent: 'outline',
+								className: '-ml-px w-fit cursor-pointer rounded-l-none'
+							})}
+							href={entry.data.link}
+							target="_blank"
+						>
+							Read Original Source
+							<IconOpenLink />
+						</a>
+					</div>
+
 					<div
 						className="mx-auto prose max-w-2xl prose-neutral xl:min-w-2xl dark:prose-invert prose-a:decoration-accent prose-a:hover:decoration-foreground prose-img:rounded-xl prose-img:border prose-img:border-border prose-img:shadow-sm prose-img:shadow-accent/50"
 						dangerouslySetInnerHTML={{
@@ -92,17 +140,6 @@ function RouteComponent() {
 									: entry.data?.content || entry.data?.description
 						}}
 					/>
-
-					<Link
-						className={buttonStyles({ intent: 'outline', className: 'mt-4 w-fit cursor-pointer' })}
-						to="/reader"
-						search={{ ...search, view: search.view === 'expanded' ? undefined : 'expanded' }}
-					>
-						{search.view === 'expanded' && content.status === 'success'
-							? 'Back to Summary'
-							: 'Read Full Article'}
-						{content.isLoading ? <Loader /> : null}
-					</Link>
 				</>
 			)}
 		</div>
