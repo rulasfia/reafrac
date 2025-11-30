30669dfb3eadbcd11f44075885932906ef66b672
Khoirul Asfian <mk.asfian@gmail.com>
2025-11-30 16:11:20 +0700

    fix: sign-up username invalid char

--- apps/web/src/lib/middleware/logger-middleware.ts
+++ apps/web/src/lib/middleware/logger-middleware.ts
@@ -2,7 +2,7 @@ import { createMiddleware } from '@tanstack/react-start';
 
 export const requestLoggerMiddleware = createMiddleware().server(async ({ next, request }) => {
 	if (process.env.NODE_ENV?.toLowerCase() !== 'production') {
-		console.log({ [request.method]: request.url });
+		console.log(`[${request.method}] - ${request.url}`);
 	}
 	return await next();
 });
