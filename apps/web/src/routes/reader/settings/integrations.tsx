3ab74db14d289e05ede078ead5f3f892b8f605a5
Khoirul Asfian <mk.asfian@gmail.com>
2025-12-05 10:42:58 +0700

    chore: improve reader fetching flow

--- apps/web/src/routes/reader/settings/integrations.tsx
+++ apps/web/src/routes/reader/settings/integrations.tsx
@@ -1,8 +1,13 @@
 import { MinifluxIntegrationSetting } from '@/components/settings/miniflux-integration';
+import { getExistingIntegrationServerFn } from '@/lib/server/integration-sfn';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/reader/settings/integrations')({
-	component: RouteComponent
+	component: RouteComponent,
+	loader: async ({ context }) => {
+		const integration = await getExistingIntegrationServerFn({ data: { userId: context.user.id } });
+		return { integration };
+	}
 });
 
 function RouteComponent() {
